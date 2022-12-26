0604f0d538aa9181105ceda8782e96a5688726b9
Accieo <42718776+Accieo@users.noreply.github.com>
2022-12-26 18:30:27 +0100

    Update for 1.19.3

--- src/main/java/accieo/striders/grace/StridersGrace.java
+++ src/main/java/accieo/striders/grace/StridersGrace.java
@@ -3,8 +3,8 @@ package accieo.striders.grace;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
 public class StridersGrace implements ModInitializer {
 
 	public static final Enchantment STRIDERS_GRACE = new StridersGraceEnchantment();
@@ -12,7 +12,7 @@ public class StridersGrace implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		Registry.register(Registry.ENCHANTMENT, new Identifier("stridersgrace", "striders_grace"), STRIDERS_GRACE);
+		Registry.register(Registries.ENCHANTMENT, new Identifier("stridersgrace", "striders_grace"), STRIDERS_GRACE);
 
 	}
 }
